ccecefaa6dd17666169d7d9e14381f65ac648fda
wkflanders <wflan@seas.upenn.edu>
2024-11-15 03:20:14 -0500

    import fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Akshar, Inconsolata } from "next/font/google";
+
+export const akshar = Akshar({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+export const inconsolata = Inconsolata({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
